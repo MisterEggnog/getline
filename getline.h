35b81d06f783168e5b464cd1f30c2e5ceb489e7e
Baldwin, Josiah <jbaldwin@nwvariancesoftware.dev>
2022-03-13 17:51:29 -0700

    Simplified header

--- getline.h
+++ getline.h
@@ -1,4 +1,3 @@
-
 #ifndef INCLUDE_GETLINE_H_C
 #define INCLUDE_GETLINE_H_C
 
@@ -6,29 +5,19 @@
 
 #include <unistd.h>
 
-#else // __unix__
+#else // not __unix__
 
 #include <stdint.h>
 
-// #include <limits.h>
-// #if SIZE_WIDTH == BITS_WIDTH
-// typedef ssize_t intBITS_t;
-
 typedef ssize_t intmax_t;
-
-#ifndef GETLINE_DOES_NOT_EXIST
-#	define GETLINE_DOES_NOT_EXIST
-#endif // GETLINE_DOES_NOT_EXIST
 
 #endif // __unix__
 
 
-#ifdef GETLINE_DOES_NOT_EXIST
-
 #include <stdio.h>
 
-ssize_t getline(char** lineptr, size_t* n, FILE* stream);
+#define getline getline_shim
 
-#endif // GETLINE_DOES_NOT_EXIST
+ssize_t getline_shim(char** lineptr, size_t* n, FILE* stream);
 
 #endif // INCLUDE_GETLINE_H_C
