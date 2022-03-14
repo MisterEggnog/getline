af63a7a5a3dc1e639d2e578779fdead6e51dcf57
Baldwin, Josiah <jbaldwin@nwvariancesoftware.dev>
2022-03-14 14:10:43 -0700

    Added test code

--- tests.c
+++ tests.c
@@ -1,8 +1,26 @@
 #include <acutest.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "getline.h"
+
+#define LINE_LEN 85
 
 void
 getline_basic(void) {
+	char exp[] = "Tiwlers\n";
+	size_t line_len = LINE_LEN;
+	char* out = malloc(line_len);
+	FILE* tmp_out = tmpfile();
+
+	fputs(exp, tmp_out);
+	rewind(tmp_out);
+
+	getline(&out, &line_len, tmp_out);
+
+	TEST_CHECK(strcmp(exp, out) == 0);
+
+	fclose(tmp_out);
 }
 
 TEST_LIST = {
