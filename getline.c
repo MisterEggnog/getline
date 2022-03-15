1b2f61e07a4d0ba626fb075d5b9b09fd4bb31c8b
Baldwin, Josiah <jbaldwin@nwvariancesoftware.dev>
2022-03-15 10:54:29 -0700

    Semi-passes tests

--- getline.c
+++ getline.c
@@ -1,10 +1,18 @@
+#include <string.h>
 #include "getline.h"
 
 #define LINE_SIZE 85
 
 ssize_t
 getline_shim(char** lineptr, size_t* n, FILE* stream) {
-	char in_line[LINE_SIZE];
+	char in_line[LINE_SIZE] = "";
 
-	fgets(/* stuff */);
+	if (fgets(in_line, sizeof in_line, stream) == NULL && strlen(in_line) == 0) {
+		**lineptr = '\0';
+		return -1;
+	}
+
+	strcpy(*lineptr, in_line);
+
+	return 0;
 }
