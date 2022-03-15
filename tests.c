37be1c391ce4fe1b35595dd1c34baa03f2f7cb8b
Baldwin, Josiah <jbaldwin@nwvariancesoftware.dev>
2022-03-15 10:54:08 -0700

    Check for return type is right size

--- tests.c
+++ tests.c
@@ -10,17 +10,21 @@ void
 getline_basic(void) {
 	char exp[] = "Tiwlers\n";
 	size_t line_len = LINE_LEN;
+	ssize_t chars_read;
 	char* out = malloc(line_len);
 	FILE* tmp_out = tmpfile();
 
 	fputs(exp, tmp_out);
 	rewind(tmp_out);
 
-	getline(&out, &line_len, tmp_out);
+	chars_read = getline(&out, &line_len, tmp_out);
 
 	TEST_CHECK(strcmp(exp, out) == 0);
 	TEST_DUMP("Expected:", exp, sizeof exp);
 	TEST_DUMP("Produced:", out, strlen(out));
+
+	TEST_CHECK(chars_read == sizeof exp);
+	TEST_MSG("getline returned %ld, rather than %ld", chars_read, sizeof exp);
 
 	fclose(tmp_out);
 	free(out);
