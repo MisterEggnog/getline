8ae0bdba4deb5a0309f54ac32a056f660c8f2f41
Baldwin, Josiah <jbaldwin@nwvariancesoftware.dev>
2022-03-15 07:54:30 -0700

    Test for program failure

--- tests.c
+++ tests.c
@@ -24,7 +24,23 @@ getline_basic(void) {
 	free(out);
 }
 
+void
+getline_fails_with_no_input(void) {
+	size_t line_len = LINE_LEN;
+	char* out = malloc(line_len);
+	FILE* tmp_out = tmpfile();
+
+	ssize_t ret_code = getline(&out, &line_len, tmp_out);
+
+	TEST_CHECK(ret_code == -1);
+	TEST_MSG("getline did not return -1 for empty input.");
+
+	fclose(tmp_out);
+	free(out);
+}
+
 TEST_LIST = {
 	{ "Basic usage", getline_basic },
+	{ "getline fails for empty file input", getline_fails_with_no_input },
 	{ NULL, NULL },
 };
