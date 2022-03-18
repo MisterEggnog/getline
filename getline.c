a7435770358f92e30b4715a867a26e203dee79f3
Baldwin, Josiah <jbaldwin@nwvariancesoftware.dev>
2022-03-17 22:21:05 -0700

    Use while loop in getline

--- getline.c
+++ getline.c
@@ -6,13 +6,19 @@
 ssize_t
 getline_shim(char** lineptr, size_t* n, FILE* stream) {
 	char in_line[LINE_SIZE] = "";
+	void* result;
 
-	if (fgets(in_line, sizeof in_line, stream) == NULL && strlen(in_line) == 0) {
-		**lineptr = '\0';
-		return -1;
-	}
+	do {
+		result = fgets(in_line, sizeof in_line, stream);
 
-	strcpy(*lineptr, in_line);
+		if (result == NULL && strlen(in_line) == 0) {
+			**lineptr = '\0';
+			return -1;
+		}
+
+		strcpy(*lineptr, in_line);
+	} while (feof(stream) == 0 && *lineptr[strlen(*lineptr)] != '\n');
+
 
 	return strlen(*lineptr);
 }
